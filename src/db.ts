03e17ec12415ad638f663f688d2942f5a82834cf
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2021-05-24 14:57:09 +0200

    db config for heroku

--- src/db.ts
+++ src/db.ts
@@ -4,9 +4,33 @@ import path from "path";
 import logger from "./logger";
 import env from "./env";
 
-const db = new Sequelize(env.DATABASE_URL, {
+const defaultOpts = {
   models: [path.join(__dirname, "/models")],
   logging: logger.debug.bind(logger)
-});
+};
+
+const opts = (nodeEnv : String) => {
+  if (nodeEnv === "production") {
+    return {
+      ...defaultOpts,
+      ...{
+        dialect: "postgres",
+        protocol: "postgres",
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false
+          }
+        }
+      }
+    };
+  }
+  return defaultOpts;
+};
+
+const db = new Sequelize(
+  env.DATABASE_URL,
+  opts(env.NODE_ENV)
+);
 
 export default db;
--- src/env.ts
+++ src/env.ts
@@ -3,10 +3,20 @@ import { load } from "ts-dotenv";
 
 const envName = process.env.NODE_ENV ?? "development";
 
+const Environment = [
+  "development" as const,
+  "test" as const,
+  "production" as const
+];
+
 const env = load({
   SESSION_SECRET: String,
   DATABASE_URL: String,
   LOG_LEVEL: String,
+  NODE_ENV: {
+    type: Environment,
+    default: "development"
+  },
   PORT: {
     type: Number,
     default: 3000
